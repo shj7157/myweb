f27c4e4ec43e465bf2284f18fdb8baf8ee7ccfdf
DESKTOP-5TDIR37\user1 <ana3630@naver.com>
2026-01-16 11:48:50 +0900

    func: 회원가입 유효성 검증

--- app/index.js
+++ app/index.js
@@ -1,3 +1,33 @@
-function f01(){
-    console.log("12354")
+function handleJoin(){
+    const userId = document.querySelector("input[name=userId]").value;
+    const userPw = document.querySelector("input[name=userPw]").value;
+    const userPw2 = document.querySelector("input[name=userPw2]").value;
+    const userNick = document.querySelector("input[name=userNick]").value;
+    
+    
+    // id
+    // 소문자 및 숫자 허용, 4~8글자
+    const idOk = /^[a-z0-9].{4,8}$/.test(userId); 
+    if(!idOk){
+        alert("ID 확인바람")
+        return false;
+    }
+    
+    // pw
+    // 숫자만 허용 4~8글자
+    const pwOk1 = /^[0-9].{4,8}$/.test(userPw);
+    const pwOk2 = userPw === userPw2
+    const pwOk = pwOk1 && pwOk2;
+    if(!pwOk){
+        alert("PW 확인바람")
+        return false;
+    }
+    
+    // nick
+    const nickOk = /^[가-힣].{2,4}$/.test(userNick);
+    if(!nickOk){
+        alert("NICK 확인바람")
+        return false;
+    }
+
 }
